8e1a7c84cb288e245ddfc284852d24ac54030dcb
David Gabrielian <davidgabrielofficial@gmail.com>
2020-10-04 15:27:10 +0400

    1.2.1

---
+++ assets/app.js
@@ -0,0 +1,7 @@
+// const play = () => {
+//   document.getElementById("landing-page").style.display = "none";
+// };
+
+// window.onload = function () {
+//   document.getElementsByClassName("loader").style.display = "none";
+// };
